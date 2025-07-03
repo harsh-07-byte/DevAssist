6d7114bc7f9eb9f0a146295534dbd12e0949bd68
Harsh Bosamiya <202201243@daiict.ac.in>
2025-07-03 20:18:30 +0530

    Frontend Home Page

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,124 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course introduces the fundamentals of HTML, focusing on how to build and organize web pages by incorporating text, hyperlinks, images, and other essential elements.",
+                level : 'Beginner',
+                lessonNumber : 6
+            },
+
+                        {
+                heading : "Responsive Web design",
+                description : "This course covers techniques in responsive web design, enabling websites to adjust seamlessly across various screen sizes and device types.",
+                level : 'Intermediate',
+                lessonNumber : 10
+            },
+
+            {
+                heading : "Learn CSS",
+                description : "This course delves into advanced concepts of HTML5 and CSS3, focusing on animations, transitions, and modern layout strategies.",
+                level : 'Beginner',
+                lessonNumber : 5
+            }
+
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "Responsive Web design",
+                description : "This course covers techniques in responsive web design, enabling websites to adjust seamlessly across various screen sizes and device types.",
+                level : 'Intermediate',
+                lessonNumber : 10
+            },
+                        {
+                heading : "Learn CSS",
+                description : "This course delves into advanced concepts of HTML5 and CSS3, focusing on animations, transitions, and modern layout strategies.",
+                level : 'Beginner',
+                lessonNumber : 5
+            },
+            {
+                heading : "Learn HTML",
+                description : "This course introduces the fundamentals of HTML, focusing on how to build and organize web pages by incorporating text, hyperlinks, images, and other essential elements.",
+                level : 'Beginner',
+                lessonNumber : 6
+            },
+
+
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "This course introduces the fundamentals of Java, focusing on how to develop and structure programs using variables, control statements, functions, classes, and other core programming constructs.",
+                level : 'Beginner',
+                lessonNumber : 10
+            },
+            {
+                heading : "Git",
+                description : "This course introduces version control using Git, empowering developers to efficiently manage code changes, collaborate on projects, and track development history across different environments.",
+                level : 'Beginner',
+                lessonNumber : 3
+            },
+              {
+                heading : "Python",
+                description : "This course explores advanced Python programming concepts, emphasizing object-oriented design, decorators, generators, and efficient data handling techniques",
+                level : 'Beginner',
+                lessonNumber : 15
+            }
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Fast API",
+                description : "This course introduces FastAPI, a modern web framework for building high-performance APIs with Python, enabling rapid development of scalable and efficient backend services",
+                level : 'Advanced',
+                lessonNumber : 20
+            },
+            {
+                heading : "Flask",
+                description : "This course introduces web development using Flask, equipping learners with the skills to build dynamic, scalable web applications with Python",
+                level : 'Advanced',
+                lessonNumber : 15
+            },
+            {
+                heading : "Djano",
+                description : "This course covers essential Django techniques, enabling developers to build dynamic, scalable web applications with efficient back-end logic and seamless database integration.",
+                level : 'Advanced',
+                lessonNumber : 18
+            }
+            
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Sanity",
+                description : "This course explores techniques in Sanity content modeling and integration, empowering developers to create flexible, scalable websites that adapt content seamlessly across platforms and devices.",
+                level : 'Advanced',
+                lessonNumber : 25
+            },
+            {
+                heading : "Docker",
+                description : "This course introduces Docker fundamentals, empowering developers to build, package, and deploy applications consistently across different environments using containerization.",
+                level : 'Intermediate',
+                lessonNumber : 15
+            },
+
+             {
+                heading : "Next.js",
+                description : "This course introduces development with Next.js, equipping learners to build fast, scalable web applications with server-side rendering, dynamic routing, and optimized performance.",
+                level : 'Beginner',
+                lessonNumber : 10
+            }
+        ]
+    },
+]
